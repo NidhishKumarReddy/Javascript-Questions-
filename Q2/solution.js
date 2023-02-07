42f57debea8472fea5d31736d3504ac1ed733b5f
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2023-02-07 18:46:00 +0530

    refactor q2 solution

--- Q2/solution.js
+++ Q2/solution.js
@@ -20,75 +20,35 @@ const object2 = {
     city: "Bangalore"
 };
 
-/***
- * validate function will take two values as parameters and returns whether the two values are same or not
- * value1, value2 can be of any type between number, string, object, array
- */
-function validate(value1, value2) {
-    if (typeof value1 === typeof value2) {
-        if (typeof value1 === "object") {
-            if (Array.isArray(value1) !== Array.isArray(value2)) {
-                return false;
-            } else if (
-                Array.isArray(value1) === Array.isArray(value2) &&
-                !Array.isArray(value1)
-            ) {
-                const result = compareObjects(value1, value2);
-                // when result === false then only return, if return true, future keys will not get executed
-                if (!result) {
-                    return result;
-                }
-            } else if (
-                Array.isArray(value1) === Array.isArray(value2) &&
-                Array.isArray(value1)
-            ) {
-                const result = compareArrays(value1, value2);
-                // when result === false then only return, if return true, future indices will not get executed
-                if (!result) {
-                    return result;
-                }
-            }
-        } else {
-            if (value1 !== value2) {
-                return false;
-            }
-        }
-    } else {
-        return false;
-    }
-}
-
-// Compare two objects deeply nested
+// This Method checks for objects, arrays, numbers, strings, null, undefined prop types
 function compareObjects(object1, object2) {
-    const length1 = Object.keys(object1).length;
-    const length2 = Object.keys(object2).length;
-    if (length1 !== length2) {
-        return false;
+    // Check whether the two params are same or not
+    if (object1 === object2) {
+        return true;
     }
-    for (const key1 in object1) {
-        const value1 = object1[key1];
-        const value2 = object2[key1];
-        const result = validate(value1, value2);
-        if (result !== undefined) {
-            return result;
-        }
-    }
-    return true;
-}
 
-// Compare two arrays deeply nested
-function compareArrays(array1, array2) {
-    const length1 = array1.length;
-    const length2 = array2.length;
-    if (length1 !== length2) {
+    // Checking the given params are objects are not
+    if (
+        object1 === null ||
+        typeof object1 !== "object" ||
+        object2 === null ||
+        typeof object2 !== "object"
+    ) {
         return false;
     }
-    for (let index = 0; index < length1; index++) {
-        const value1 = array1[index];
-        const value2 = array2[index];
-        const result = validate(value1, value2);
-        if (result !== undefined) {
-            return result;
+
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+
+    // Loop through the object1 keys
+    for (const key of keys1) {
+        // Find the whether current key in keys1 exited in keys2 also
+        if (!keys2.includes(key)) {
+            return false;
+        }
+        // If current key is there in both keys1 and keys2 call recursion
+        if (!compareObjects(object1[key], object2[key])) {
+            return false;
         }
     }
     return true;
